5ba867f92cf9e8aebc2bdb9d7533ac9cebf027c5
xa_lixin <xa_lixin@sunseer.co.jp>
2017-04-11 18:57:31 +0800

    注册页面表单验证逻辑添加

--- kankan/js/language.js
+++ kankan/js/language.js
@@ -2,12 +2,16 @@
 	mui.plusReady(function() {
 		var language = plus.os.language;
 		console.log("current language is " + language);
-		var dynamic = document.createElement("script");
+		var dynamic1 = document.createElement("script");
+		var dynamic2 = document.createElement("script");
 		if(!language || language == "ja_JP") {
-			dynamic.src="../../js/language/locale_ja_JP.js";
+			dynamic1.src="../../js/language/locale_ja_JP.js";
+			dynamic2.src="../../js/validation/localization/messages_ja.js";
 		} else {
-			dynamic.src="../../js/language/locale_zh_CN.js";
+			dynamic1.src="../../js/language/locale_zh_CN.js";
+			dynamic2.src="../../js/validation/localization/messages_zh.js";
 		}
-		document.head.appendChild(dynamic);
+		document.head.appendChild(dynamic1);
+		document.head.appendChild(dynamic2);
 	});
 })(mui, document);
