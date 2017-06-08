579c0cb61d1d924e3b15c138bec173440271a2ce
xa_fangyf <xa_fangyf@sunseer.co.jp>
2017-06-08 17:29:17 +0800

    商品选择页面修改

--- kankan/js/language/locale_ja_JP.js
+++ kankan/js/language/locale_ja_JP.js
@@ -3,22 +3,22 @@ var JSLocale = {
 	"back": "戻る",
 
 	/*login page message begin*/
-	"login": "登録",
-	"wechat_login": "Wechatで登録する",
-	"line_login": "Lineで登録する",
-	"facebook_login": "Facebookで登録する",
-	"twitter_login": "Twitterで登録する",
-	"email_login": "メールアドレスで登録する",
+	"login": "ログイン",
+	"wechat_login": "Wechatでログイン",
+	"line_login": "Lineでログイン",
+	"facebook_login": "Facebookでログイン",
+	"twitter_login": "Twitterでログイン",
+	"email_login": "メールアドレスでログイン",
 	"forget_password": "パスワードを忘れました",
 	/*login page message end*/
 
 	/*user login page message begin*/
-	"user-login": "ユーザー登録",
+	"user-login": "ログイン",
 	"email": "<span style='color:red;'>*</span>メールアドレス",
 	"username": "<span style='color:red;'>*</span>ユーザー名",
 	"password": "<span style='color:red;'>*</span>パスワード",
 	"new_password": "<span style='color:red;'>*</span>新しいパスワード",
-	"password-confirm": "<span style='color:red;'>*</span>パスワード再入力",
+	"password-confirm": "<span style='color:red;'>*</span>確認パスワード",
 	"address": "<span style='color:red;'>*</span>都通府県",
 	"address-details": "未設定",
 	"introduction-code": "招待コード(任意)",
@@ -33,10 +33,10 @@ var JSLocale = {
 	"please-input-verification-code": "コードを入力",
 	"send-verification-code": "認証コードを発信",
 	"retrieve_password":"パスワード再発行",
-	"forget_password_msg": "カンカンで登録しているE-mailを入力してください 入力頂いたE-mailに新しいパスワードを送信します",
+	"forget_password_msg": "登録済みE-mailを入力してください 新しいパスワードを送信します",
 	/*user login page message end*/
 	/*detail page*/
-	"det_spdescription": "商品説明 ",
+	"det_spdescription": "商品詳細 ",
 	"det_spahare": "シェア",
 	"det_goodconcel": "キャンセル",
 	"det_goods": "いいね!",
@@ -55,23 +55,23 @@ var JSLocale = {
 	"det_normalcomments": "普通",
 	"det_badcomments": "悪い",
 	"det_all": "全て",
-	"comdity_null": "暫時データ",
-	"contentlength": "レビューの内容(300字以内)",
+	"comdity_null": "データなし",
+	"contentlength": "レビュー内容(300文字以内)",
 	"contentsend": "送 信",
 
 	// Home Page
 	"app_name": "カンカン",
 	"tab_1": "ホーム",
-	"tab_2": "登録",
-	"tab_3": "求む",
+	"tab_2": "出品",
+	"tab_3": "求める商品",
 	"tab_4": "物流",
 
 	//commodity login page
 	"commodity_login": "商品登録",
 	"commodity_photos": "商品写真",
 	"commodity_photodet": "この画像を削除しますか？",
-	"commodity_name": "商品名と説明",
-	"commodity_namelength": "商品名（必須４０文字まで）",
+	"commodity_name": "商品名と商品詳細",
+	"commodity_namelength": "商品名（必須：40文字まで）",
 	"commodity_descriptionlength": "商品説明(必須：300文字まで)",
 	"commodity_detail": "商品詳細",
 	"commodity_sorts": "カテゴリー",
@@ -98,14 +98,14 @@ var JSLocale = {
 	"introducetext":"紹介文",
 	"userlanguage":"言語",
 	"jpanese_user":"日本語",
-	"chinese_user":"漢語",
-	"english_user":"英語",
-	"korean_user":"韓国語",
+	"chinese_user":"中国語",
+	"english_user":"Enlish",
+	"korean_user":"한국의",
 	"infoeditetitle":"プロフィール編集",
 	//logisticslist page
 	"sendmes":"はい",
 	"logistic":"物流",
-	
+
 	//point change to ticket page
 	"point_to_ticket_title":"チケット購入",
 	"point_to_ticket_num1":"チケット1枚",
@@ -113,12 +113,12 @@ var JSLocale = {
 	"point_to_ticket_num7":"チケット7枚(5枚+2枚)",
 	"point_to_ticket_num15":"チケット15枚(10枚+5枚)",
 	"buy":"購入",
-	
+
 	//buy point page
 	"change":"チャージ",
 	//request
 	"requesttitle":"リクエスト",
-	"requesthistory":"交易履历",
+	"requesthistory":"取引履历",
 	"requestedpro":"リクエストされた",
 	"requestpro":"リスエストした",
 	//setting page
@@ -139,21 +139,21 @@ var JSLocale = {
 	"Appeal":"お問い合わせ",
 	"musttext":"印がある項目は必須項目となります。",
 	"upimgtext":"画像をアップロード",
-	"nametext":"姓名",
+	"nametext":"名前",
 	"contenttext":"お問い合わせ内容",
 	"suretext":"確    認",
-	"writeemail":"郵便箱",
-	"emailwtext":"どうぞ完全メールアドレス情報",
+	"writeemail":"メール",
+	"emailwtext":"正しいメールアドレスを入力してください",
 	"new_user_login":"新しいはじめる方はこちら",
-	"transaction":"交易",
+	"transaction":"取引",
 	"trans_criterion":"特定商取引法に基づく表記",
 	"trans_logistic":"運送会社に連絡する",
-	'trans_finish':"交易終止 ",
+	'trans_finish':"取引終止 ",
 	'eve_comment':"評価のコメントを記入しましょう ",
 	'eve_btn':"評価する",
 	"home_return":"ホームに戻る",
 	//message
-	"messagetitle":"構内信",
+	"messagetitle":"お知らせ",
 }
 
 $(function() {
