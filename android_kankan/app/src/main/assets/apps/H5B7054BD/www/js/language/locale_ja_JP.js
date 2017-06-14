4a57601a7ba5b9a491641031864c5d960b0a325a
xa_lixin <xa_lixin@sunseer.co.jp>
2017-06-14 10:42:23 +0800

    android打包程序更新

--- android_kankan/app/src/main/assets/apps/H5B7054BD/www/js/language/locale_ja_JP.js
+++ android_kankan/app/src/main/assets/apps/H5B7054BD/www/js/language/locale_ja_JP.js
@@ -3,40 +3,40 @@ var JSLocale = {
 	"back": "戻る",
 
 	/*login page message begin*/
-	"login": "登録",
-	"wechat_login": "Wechatで登録する",
-	"line_login": "Lineで登録する",
-	"facebook_login": "Facebookで登録する",
-	"twitter_login": "Twitterで登録する",
-	"email_login": "メールアドレスで登録する",
-	"forget_password": "パスワードを忘れました",
+	"login": "ログイン",
+	"wechat_login": "Wechatでログイン",
+	"line_login": "Lineでログイン",
+	"facebook_login": "Facebookでログイン",
+	"twitter_login": "Twitterでログイン",
+	"email_login": "メールアドレスでログイン",
+	"forget_password": "パスワードを忘れてしまった方はこちら",
 	/*login page message end*/
 
 	/*user login page message begin*/
-	"user-login": "ユーザー登録",
-	"email": "メールアドレス",
-	"username": "ユーザー名",
-	"password": "パスワード",
-	"new_password": "新しいパスワード",
-	"password-confirm": "パスワード再入力",
-	"address": "都通府県",
+	"user-login": "ログイン",
+	"email": "<span style='color:red;display:inline;padding:0px'>*</span>メールアドレス",
+	"username": "<span style='color:red;'>*</span>ユーザー名",
+	"password": "<span style='color:red;'>*</span>パスワード",
+	"new_password": "<span style='color:red;'>*</span>新しいパスワード",
+	"password-confirm": "<span style='color:red;'>*</span>確認パスワード",
+	"address": "<span style='color:red;'>*</span>都通府県",
 	"address-details": "未設定",
 	"introduction-code": "招待コード(任意)",
-	"verification-code": "確認コード",
+	"verification-code": "<span style='color:red;'>*</span>パスワード再入力",
 	"register": "確定",
 	"register-tips": "あとからプロフィールは変更可能です",
 	"please-input-email": "メールアドレス",
 	"please-input-username": "アプリ内に表示されます",
-	"please-input-password": "半角英文字3文字以上20文字以内",
+	"please-input-password": "パスワードを設定用文字列の長さは6桁以上、16桁以下してください",
 	"please-confirm-password": "パスワード確認",
 	"please-input-introduction-code": "半角英数字6位",
 	"please-input-verification-code": "コードを入力",
 	"send-verification-code": "認証コードを発信",
 	"retrieve_password":"パスワード再発行",
-	"forget_password_msg": "カンカンで登録しているE-mailを入力してください 入力頂いたE-mailに新しいパスワードを送信します",
+	"forget_password_msg": "カンカンで登録しているメールアドレスを入力してください。登録されているメールアドレスに新しいパスワード情報のメールを送信します。",
 	/*user login page message end*/
 	/*detail page*/
-	"det_spdescription": "商品説明 ",
+	"det_spdescription": "商品詳細 ",
 	"det_spahare": "シェア",
 	"det_goodconcel": "キャンセル",
 	"det_goods": "いいね!",
@@ -55,24 +55,24 @@ var JSLocale = {
 	"det_normalcomments": "普通",
 	"det_badcomments": "悪い",
 	"det_all": "全て",
-	"comdity_null": "暫時データ",
-	"contentlength": "レビューの内容（300字を超えない）",
+	"comdity_null": "データなし",
+	"contentlength": "レビュー内容(300文字以内)",
 	"contentsend": "送 信",
-
+	"evalute_finish":"取引完了",
 	// Home Page
 	"app_name": "カンカン",
 	"tab_1": "ホーム",
-	"tab_2": "登録",
-	"tab_3": "求め",
-	"tab_4": "物流",
+	"tab_2": "出品",
+	"tab_3": "求める商品",
+	"tab_4": "運送",
 
 	//commodity login page
 	"commodity_login": "商品登録",
 	"commodity_photos": "商品写真",
 	"commodity_photodet": "この画像を削除しますか？",
-	"commodity_name": "商品名と説明",
-	"commodity_namelength": "商品名（必須４０文字まで）",
-	"commodity_descriptionlength": "商品説明（1000文字まで）",
+	"commodity_name": "商品名と商品詳細",
+	"commodity_namelength": "商品名（必須：40文字まで）",
+	"commodity_descriptionlength": "商品説明(必須：300文字まで)",
 	"commodity_detail": "商品詳細",
 	"commodity_sorts": "カテゴリー",
 	"commodity_must": "必須",
@@ -83,10 +83,10 @@ var JSLocale = {
 	"commodity_pointbuy": "ポイント",
 	"commodity_buttonsure": "出品する",
 	"commodity_price": "価  格",
-	"commodity_forbidentext": "禁止されている出品、行為を必ず確認ください。またブランド品でシリアルナンバー等がある場合はご記載ください。偽ブランドの販売は犯罪であり処罰される可能性があります。",
-	"commodity_want":"物探す登録",
+	"commodity_forbidentext": "出品が禁止されている商品、禁止されている行為を必ず確認してください。またブランド品でシリアルナンバー等がある場合はご記載ください。偽ブランド品の販売は犯罪であり処罰される可能性があります。",
+	"commodity_want":"欲しい物登録",
 	"commodity_want_butonsure":"投稿する",
-	"commodity_want_forbidentext":"法律に禁止されているもの或は非常識ものを登録しないで下さい、投稿した情報の有効期限は一ヶ月内になりますので、ご了承ください。",
+	"commodity_want_forbidentext":"法律で禁止されているもの或いは非常識なものを登録しないでください、投稿した情報の有効期限は一か月間となりますので、ご了承ください。",
 	//user page
 	"ticknumber":"チケット数",
 	"userinforbutntext":"プロフィール編集",
@@ -94,18 +94,18 @@ var JSLocale = {
 	"onshowingtext":"出品中アイテム",
 	"praiseprotext":"いいねしたアイテム",
 	"favoriteprotext":"気になる",
-	"uinfousername":"ユーザ名",
+	"uinfousername":"ユーザー名",
 	"introducetext":"紹介文",
 	"userlanguage":"言語",
 	"jpanese_user":"日本語",
-	"chinese_user":"漢語",
-	"english_user":"英語",
-	"korean_user":"韓国語",
+	"chinese_user":"中国語",
+	"english_user":"Enlish",
+	"korean_user":"한국의",
 	"infoeditetitle":"プロフィール編集",
 	//logisticslist page
-	"sendmes":"は",
-	"logistic":"物流",
-	
+	"sendmes":"はい",
+	"logistic":"運送会社",
+
 	//point change to ticket page
 	"point_to_ticket_title":"チケット購入",
 	"point_to_ticket_num1":"チケット1枚",
@@ -113,12 +113,12 @@ var JSLocale = {
 	"point_to_ticket_num7":"チケット7枚(5枚+2枚)",
 	"point_to_ticket_num15":"チケット15枚(10枚+5枚)",
 	"buy":"購入",
-	
+
 	//buy point page
 	"change":"チャージ",
 	//request
 	"requesttitle":"リクエスト",
-	"requesthistory":"交易履历",
+	"requesthistory":"取引履历",
 	"requestedpro":"リクエストされた",
 	"requestpro":"リスエストした",
 	//setting page
@@ -133,19 +133,50 @@ var JSLocale = {
 	"overview":"会社概要",
 	"logout":"ログアウト",
 	"version":"バージョン",
+	"moneycla":"資金決済法",
+	"speciallow":"特定商取引法",
 	//question page
 	"Appeal":"お問い合わせ",
 	"musttext":"印がある項目は必須項目となります。",
 	"upimgtext":"画像をアップロード",
-	"nametext":"姓名",
+	"nametext":"氏名",
 	"contenttext":"お問い合わせ内容",
-	"suretext":"確    認"
+	"suretext":"確    認",
+	"writeemail":"メール",
+	"emailwtext":"正しいメールアドレスを入力してください",
+	"new_user_login":"会員登録はこちら",
+	"transaction":"取引",
+	"trans_criterion":"特定商取引法に基づく表記",
+	"trans_logistic":"運送会社に連絡する",
+	'trans_finish':"取引終止 ",
+	'eve_comment':"評価のコメントを記入しましょう ",
+	'eve_btn':"評価する",
+	"home_return":"ホームに戻る",
+	//message
+	"messagetitle":"あなたへのお知らせ",
+	"stautestext":"商品取引中",
+	"thistorytext":"取引履歴",
+	//category
+	"category_top_1":"すべて",
+	"category_top_2":"求める商品",
+	"category_top_3":"レディース",
+	"category_top_4":"メンズ",
+	"category_top_5":"ベビー・キッズ",
+	"category_top_6":"インテリア・住まい・小物",
+	"category_top_7":"エンタメ・ホビー",
+	"category_top_8":"コスメ・香水・美容",
+	"category_top_9":"家電・スマホ・カメラ",
+	"category_top_10":"スポーツ・レジャー",
+	"category_top_11":"ハンドメイド",
+	"category_top_12":"チケット・クーポン",
+	"category_top_13":"自動車・オートバイ",
+	"category_top_14":"その他"
 }
 
 $(function() {
 	$('.lang').not('input').each(function(e) {
 		var msg_key = $(this).attr('data-locale');
-		$(this).text(JSLocale[msg_key]);
+		$(this).html(JSLocale[msg_key]);
 	});
 
 	$('input.lang').each(function(e) {
